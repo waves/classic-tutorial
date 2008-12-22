0fcd528b31cfc9da7bc28ae03d66eadf2e017d0c
Matthew King <automatthew@gmail.com>
2008-12-22 14:37:19 -0600

    model files for Entry and Comment

---
+++ models/comment.rb
@@ -0,0 +1,18 @@
+module Blog
+  module Models
+    class Comment < Default
+      many_to_one :entry, :class => Blog::Models::Entry
+      before_save do
+        set_with_params(:updated_on => Time.now)
+      end
+      
+      def time
+        updated_on.strftime('%I:%M %p on %b %d, %Y')
+      end
+      
+      def attribution
+        name.nil? || name.empty?  ? 'anonymous coward' : name
+      end
+    end
+  end
+end
---
+++ models/entry.rb
@@ -0,0 +1,26 @@
+module Blog
+  module Models
+    class Entry < Default
+      one_to_many :comments, :class => Blog::Models::Comment, :key => :entry_id, :order => :updated_on
+      before_save do
+        set_with_params(:updated_on => Time.now)
+      end
+      
+      def date
+        updated_on.strftime('%b %d, %Y')
+      end
+      
+      def comment_number
+        n = comments.size
+        case n
+        when 0
+          "No Comments"
+        when 1
+          "1 Comment"
+        else
+          "#{n} Comments"
+        end
+      end
+    end
+  end
+end
